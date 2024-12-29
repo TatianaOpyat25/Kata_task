4f54d64e3c652b67f4bac371c7ea9aa786584ddc
tanusik/med <tanusik.med@gmail.com>
2024-12-29 12:45:48 +0300

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import static java.awt.SystemColor.text;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -40,7 +42,43 @@ public class Main {
 //            }
 //        }
 //        System.out.println(array.length);
-        System.out.println(sb);
+//        System.out.println(sb);
         return "end";
+
+        StringBuilder finalText = new StringBuilder();
+        int sumOfDigits = 0;
+        char[] chars = sb.toString().toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '-') {
+                if (i > 0 && i < chars.length - 1) {
+                    // Меняем местами символы
+                    char leftChar = chars[i - 1];
+                    char rightChar = chars[i + 1];
+                    sb.deleteCharAt(sb.length() - 1); // Удаляем последний добавленный символ
+                   sb.append(rightChar).append(leftChar);
+                    i++;
+                    // Пропускаем следующий символ, так как мы его уже обработали
+                }
+            } else if (chars[i] == '+') {
+               sb.append('!');
+            } else if (Character.isDigit(chars[i])) {
+                sumOfDigits += Character.getNumericValue(chars[i]);
+            } else {
+                sb.append(chars[i]);
+            }
+        }
+
+        // 3. Добавление суммы цифр в конец строки
+        if (sumOfDigits > 0) {
+           sb.append(" ").append(sumOfDigits);
+        }
+
+        // Вывод результата
+        System.out.println(sb.toString());
     }
+
 }
+
+
+
