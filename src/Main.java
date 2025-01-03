72b165e9f61dc6322970c0b9df5e5005cd003a51
Tatiana <tanusik.med@gmail.com>
2025-01-03 17:44:26 +0300

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -26,32 +26,39 @@ public class Main {
 
         int sumOfDigits = 0;
         char[] chars = sb.toString().toCharArray();
-        StringBuilder finalString = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
         for (int i = 0; i < chars.length; i++) {
             if (chars[i] == '-') {
                 if (i != 0 && i != chars.length - 1) {
                     // Меняем местами символы
                     char leftChar = chars[i - 1];
                     char rightChar = chars[i + 1];
-                    finalString.deleteCharAt(finalString.length() - 1); // Удаляем последний добавленный символ
-                    finalString.append(rightChar).append(leftChar);
+                    sb2.deleteCharAt(sb2.length() - 1); // Удаляем последний добавленный символ
+                    sb2.append(rightChar).append(leftChar);
                     i++;
-                    // Пропускаем следующий символ, так как мы его уже обработали
+                    // В случае если это не цифра, пропускаем следующий символ, так как мы его уже обработали
                 }
-            } else if (chars[i] == '+') {
-                finalString.append('!');
-            } else if (Character.isDigit(chars[i])) {
-                sumOfDigits += Character.getNumericValue(chars[i]);
             } else {
-                finalString.append(chars[i]);
+                sb2.append(chars[i]);
+            }
+        }
+        char[] chars2 = sb2.toString().toCharArray();
+        StringBuilder result = new StringBuilder();
+        for (char c : chars2) {
+            if (c == '+') {
+                result.append('!');
+            } else if (Character.isDigit(c)) {
+                sumOfDigits += Character.getNumericValue(c);
+            } else {
+                result.append(c);
             }
         }
         // 3. Добавление суммы цифр в конец строки
         // ВОПРОС: Из текста задания не понятно надо ли выводить ноль в конце, если в тексте встречались только ноли. Сейчас реализовано так, что ноль не выводится в любом случае.
         if (sumOfDigits > 0) {
-            finalString.append(" ").append(sumOfDigits);
+            result.append(" ").append(sumOfDigits);
         }
-        return finalString.toString();
+        return result.toString();
     }
 }
 
